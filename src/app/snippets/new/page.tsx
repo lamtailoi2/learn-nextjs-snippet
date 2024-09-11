e1cc66a204ebbd81885de990bf48ed679847fba4
Lamtailoi2 <lamtailoi11141@gmail.com>
2024-09-11 21:42:52 +0700

    init

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,10 @@
+import SnippetFrom from "@/components/SnippetForm";
+
+export default function SnippetNewPage() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <SnippetFrom />
+    </div>
+  );
+}
